34450f86c4bcd633cc2007ba5f170f0cdd96b9b6
Lasse Iversen <lasse.iver@gmail.com>
2014-06-05 20:35:30 +0200

    Changed comments

--- Simple Timer Template/TimerViewController.m
+++ Simple Timer Template/TimerViewController.m
@@ -112,7 +112,7 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated
 }
 
 @end
